0eba0bf560ae625fbe43f92efcb0d8f98a636fbc
victoricoma <victor.icoma@fatec.sp.gov.br>
2025-09-15 20:30:33 -0300

    Deploy as 20h30 dia 15 de setembro

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,11 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../firebase/firebase";
 import Banner from "./Banner";
+import Navegador from "./Navegador";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import infograficoresponsavel from '../../public/infograficorespo1.png';
 
 export default function Login() {
     const [email, setEmail] = useState("");
@@ -25,16 +30,32 @@ export default function Login() {
 
 
     return (
-        <div className="card">
-            <Banner />
-            <h1>Login</h1>
-            <form onSubmit={onSubmit} className="form">
-                <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                {error && <div className="error">{error}</div>}
-                <button type="submit">Entrar</button>
-            </form>
-            <p>Não tem conta? <Link to="/register">Registre-se</Link></p>
-        </div>
+        <Container>
+            <Row>
+                <Navegador />
+            </Row>
+            <Row>
+                <Col lg="auto" >
+                <Banner />
+                </Col>
+                <Col>
+                    <div className="card" style={{marginRight: "auto", marginLeft:"auto"}}>
+                        <h1>Entre com sua conta:</h1>
+                        <form onSubmit={onSubmit} className="form">
+                            <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                            <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                            {error && <div className="error">{error}</div>}
+                            <button type="submit">Entrar</button>
+                        </form>
+                        <p>Não tem conta? <Link to="/register">Registre-se</Link></p>
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <img src={infograficoresponsavel} 
+                width={"auto"}
+                height={"auto"} />
+            </Row>
+        </Container>
     );
 }
--- src/pages/Navegador.tsx
+++ src/pages/Navegador.tsx
@@ -3,46 +3,46 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Auth from "../service/Auth";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 export default function Navegador() {
 
   const currentUser = Auth();
-  const navigate = useNavigate();
 
-return (
-<>
-    <Navbar expand="lg" className="mb-3" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand>
-        <img src={currentUser?.photoURL || "https://via.placeholder.com/120"} width="60" height="60" alt="2Get Companion" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Infográfico</Nav.Link>
-            <Nav.Link href="#link">Quem é a turma</Nav.Link>
-            <NavDropdown title="Teoria base" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Jhon Bowlby (Teoria do Apego)</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Arthur de Schopenhauer (Paradigma do Porco Espinho)
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Machine Learning</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Onde armezenamos nosso conhecimento?
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav className="align-items-center">
-            <Link to="/profile" style={{ textDecoration: 'none', color: 'inherit' }} className='me-3'>
-              {currentUser?.displayName || "Usuário"}
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-</>
-);
+  return (
+    <>
+      <Navbar expand="lg" className="mb-3" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>
+            {currentUser ? (<img src={currentUser?.photoURL || "https://www.instagram.com/2get_a/"} width="60" height="60" alt="2Get Companion" />) :
+              (<img src="../../Logo Simples.png" width="60" height="60" alt="2Get Companion" />)}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Infográfico</Nav.Link>
+              <Nav.Link href="#link">Quem é a turma</Nav.Link>
+              <NavDropdown title="Teoria base" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Jhon Bowlby (Teoria do Apego)</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Arthur de Schopenhauer (Paradigma do Porco Espinho)
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Machine Learning</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Onde armezenamos nosso conhecimento?
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav className="align-items-center">
+              <Link to="/profile" style={{ textDecoration: 'none', color: 'inherit' }} className='me-3'>
+                {currentUser?.displayName || ""}
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
